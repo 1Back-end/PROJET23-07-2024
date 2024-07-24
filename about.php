0854ee00e955f86d9ac4303b2ea0c08ac92fcc72
Laurent Alphonse <laurentalphonsewilfried@gmail.com>
2024-07-24 11:42:04 +0200

    "Updated about.php to replace partner section with destinations section featuring Canada, USA, and Mexico, and added corresponding styles to style.css"

--- about.php
+++ about.php
@@ -106,21 +106,53 @@
         </div>
 </div>
 
-
-<?php include("section_faq.php");?>
-
-
-
 <div class="container pb-5 mt-3 p-3">
-<div class="row text-center mb-4">
-        <div class="col-md-12 col-sm-12">
-            <h2 class="animate__animated animate__fadeIn">Nos partenaires</h2>
-            <p class="small text-center">Chez <b>World Travel Company</b>, nous collaborons avec des partenaires de confiance <br> pour vous offrir des expériences de voyage exceptionnelles. </p>
+        <div class="row text-center mb-4">
+            <div class="col-md-12 col-sm-12">
+                <h2 class="animate__animated animate__fadeIn">Nos destinations</h2>
+                
+            </div>
+        </div>
+        <div class="row">
+            <!-- Carte pour le Canada -->
+            <div class="col-md-4 col-sm-12 mb-3">
+                <div class="card h-100 p-3 text-center">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/cf/Flag_of_Canada.svg/1200px-Flag_of_Canada.svg.png" class="card-img-top" alt="Drapeau du Canada">
+                    <div class="card-body">
+                        <h5 class="card-title">Canada</h5>
+                        <p class="text-muted">Explorez les vastes paysages et les villes dynamiques du Canada, de Vancouver à Toronto.</p>
+                        <a href="services_canada.php" class="btn-services animate__animated animate__fadeInUp">Plus d'infos</a>
+                    
+                    </div>
+                </div>
+            </div>
+            <!-- Carte pour les USA -->
+            <div class="col-md-4 col-sm-12 mb-3">
+                <div class="card h-100 p-3 text-center">
+                    <img src="https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1200px-Flag_of_the_United_States.svg.png" class="card-img-top" alt="Drapeau des USA">
+                    <div class="card-body">
+                        <h5 class="card-title">USA</h5>
+                        <p class="text-muted">Découvrez les États-Unis avec ses merveilles naturelles et ses grandes villes comme New York et Los Angeles.</p>
+                        <a href="services_usa.php" class="btn-services animate__animated animate__fadeInUp">Plus d'infos</a>
+                    
+                    </div>
+                </div>
+            </div>
+            <!-- Carte pour le Mexique -->
+            <div class="col-md-4 col-sm-12 mb-3">
+                <div class="card h-100 p-3 text-center">
+                    <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/1200px-Flag_of_Mexico.svg.png" class="card-img-top" alt="Drapeau du Mexique">
+                    <div class="card-body">
+                        <h5 class="card-title">Mexique</h5>
+                        <p class="text-muted">Profitez des plages ensoleillées et des richesses culturelles du Mexique, de Cancún à Mexico.</p>
+                        <a href="services_mexique.php" class="btn-services animate__animated animate__fadeInUp">Plus d'infos</a>
+                    
+                    </div>
+                </div>
+            </div>
+           
         </div>
     </div>
-
-</div>
-
 
 
 <?php include("footer.php");?>
